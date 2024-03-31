5c41e871f4786f06d835a2048c163095f46b771d
Mihailqwefr <babanovmiail29@gmail.com>
2024-03-31 17:27:47 +0300

    добавил калькулятор с операциями

---
+++ WebApplicationLesson1/Controllers/CalculatorController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplicationLesson1.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        public string Index(int a, int b, string c = "+")
+        {
+            switch (c)
+            {
+                case "+":
+                    return $"{a} + {b} = {a + b}";
+                case "-":
+                    return $"{a} - {b} = {a - b}";
+                case "*":
+                    return $"{a} * {b} = {a * b}";
+            }
+            return "Операция не конает";
+        }
+    }
+}
